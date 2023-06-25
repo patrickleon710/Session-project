0016bc9dd92a294735511844d5362dca5f6dea9b
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-25 17:30:12 +0300

    commit 1 adding a footer.jsx

---
+++ Session-project/src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>&copy; Coding Titans Group 2023 All rights reserved</p>
+            </footer>
+        </div>
+    )
+}
