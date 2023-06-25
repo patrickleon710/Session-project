88a19e1eeb26520cd860868dd6b158733aea68ca
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-25 17:46:18 +0300

    Navbar with all the links

--- Session-project/components/Navbar.jsx
+++ Session-project/components/Navbar.jsx
@@ -2,9 +2,9 @@ export default function Navbar() {
     return (
         <nav>
             <h1>Coding Titans</h1>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#projects">Projects</a></li>            
+            <li><a href="#about">About</a></li>            
         </nav>
     )
 }
