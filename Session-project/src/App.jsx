eb6bc03b4a7bb6d175c220f8dbf35c3a5b754b21
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-25 17:30:36 +0300

    Commit 2 Adding the Footer component

--- Session-project/src/App.jsx
+++ Session-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from '../components/Navbar'
 import './App.css'
+import Footer from "./Footer"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   )
 }
