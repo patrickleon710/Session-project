18dcd016a5bc6f7515926301505504eae3900763
Willylaisa <laisawilly7@gmail.com>
2023-06-25 18:04:48 +0300

    Added import Projects

--- Session-project/src/App.jsx
+++ Session-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from "../components/Footer"
+import Projects from '../components/Projects'
 import About from '../components/About'
 import './App.css'
 
