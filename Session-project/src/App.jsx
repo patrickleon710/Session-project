795a1ea9a6a89d36e34f35d765343f1b121dddea
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-25 17:37:36 +0300

    Adding th about component to App.jsx

--- Session-project/src/App.jsx
+++ Session-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from '../components/Navbar'
 import './App.css'
 import Footer from "./Footer"
+import About from './About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <About />
       <Footer />
     </>
   )
